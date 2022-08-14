3b1569571f7b6ff706543c215971494d653523b3
agustin <agustinvillagran98@gmail.com>
2022-08-14 04:09:40 -0300

    enrutamiento templates

--- render_templates/urls.py
+++ render_templates/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("", views.billetera, name="billetera"),
+    path("tubanco/", views.tubanco, name="tubanco"),
+    path("tubanco/compraventausd/", views.compraventausd, name="compraventausd"),
+    path("graficos/", views.graficos, name="graficos")
 ]
--- render_templates/views.py
+++ render_templates/views.py
@@ -17,3 +17,20 @@ def billetera(request):
     return render(
         request, "render_templates/billetera.html", {"name": request.user.first_name}
     )
+@login_required(login_url="/home/")
+def tubanco(request):
+    if not request.user.is_authenticated:
+        home(request)
+    return render( request, "render_templates/tubanco.html")
+
+@login_required(login_url="/home/")
+def compraventausd(request):
+    if not request.user.is_authenticated:
+        home(request)
+    return render( request, "render_templates/compraventausd.html")    
+
+@login_required(login_url="/home/")
+def graficos(request):
+    if not request.user.is_authenticated:
+        home(request)
+    return render( request, "render_templates/graficos.html")    
