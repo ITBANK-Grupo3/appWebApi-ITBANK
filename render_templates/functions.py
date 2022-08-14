2fc1d4407b2e6f80f96b48124f3dd50914491f22
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-08-14 15:56:15 -0300

    muestra del balance que tiene en la caja de ahorro en pesos

---
+++ render_templates/functions.py
@@ -0,0 +1,49 @@
+from database import models as models_db
+
+
+def get_user_info(dni):
+    """
+    Funcion que retorna la informacion almacenada relevante del
+    usuario
+
+    args: dni (entero)
+
+    return: diccionario con los datos del usuario
+    """
+    # Obtenemos los datos del cliente
+    cliente = models_db.Cliente.objects.using("homebanking").filter(customer_dni=dni)
+
+    diccionario = {}
+
+    if len(cliente) > 0:
+        customer_id = cliente[0].customer_id
+
+        # Recoleccion datos de las diferentes cuentas
+        caja_pesos = (
+            models_db.Cuenta.objects.using("homebanking")
+            .filter(customer_id=customer_id)
+            .filter(tipo_cuenta=1)
+        )
+
+        diccionario = {
+            "customer_id": customer_id,
+            "caja_pesos": caja_pesos[0],
+        }
+
+        # Buscar cuenta segun tipo de cliente
+        if cliente[0].tipo_cliente == 2 or cliente[0].tipo_cliente == 3:
+            caja_dolares = (
+                models_db.Cuenta.objects.using("homebanking")
+                .filter(customer_id=customer_id)
+                .filter(tipo_cuenta=2)
+            )
+            cuenta_corriente = (
+                models_db.Cuenta.objects.using("homebanking")
+                .filter(customer_id=customer_id)
+                .filter(tipo_cuenta=3)
+            )
+            # Cargamos las cuentas en el diccionario
+            diccionario["caja_dolares"] = caja_dolares[0]
+            diccionario["cuenta_corriente"]: cuenta_corriente[0]
+
+    return diccionario
