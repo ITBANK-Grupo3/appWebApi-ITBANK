c53af6ca3579c13b7547842991ac89a395b78742
agustin <agustinvillagran98@gmail.com>
2022-08-29 13:34:11 -0300

    dir_api

--- database/api/views/direcciones_viewsets.py
+++ database/api/views/direcciones_viewsets.py
@@ -45,7 +45,21 @@ class DireccionesViewSet(viewsets.ModelViewSet, CustomMethods):
             return Response(serializer.data, status=status.HTTP_201_CREATED) 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def delete(self, request, dir_id): 
+        direccion = Direcciones.objects.filter(pk=dir_id).first()
+        if direccion:
+            serializer = DireccionesSerializer(direccion)
+            direccion.delete()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND) 
     
+    def put(self, request, dir_id):
+        direccion = Direcciones.objects.filter(pk=dir_id).first()
+        serializer = DireccionesSerializer(direccion)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 """ from ..serializers import DireccionesSerializer
 from ...models import Direcciones
